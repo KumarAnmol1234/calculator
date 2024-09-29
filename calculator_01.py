4db3e9c0fd1c98b11e5f7a07cc748dc10c035c5b
dnrkranmol <dnrkranmol@gmail.com>
2024-09-29 13:38:42 +0530

    buttons

--- calculator_01.py
+++ calculator_01.py
@@ -7,6 +7,9 @@ root.geometry("290x445")
 root.title("Calculator")
 root.attributes("-alpha",0.9)
 
+
+
+
 border= Frame(root,bg="gray",borderwidth=2)
 border.pack(side= TOP,fill=X)
 a=Label (border,text="This is a sample code by @kr.anmol1234",bg="gray")
