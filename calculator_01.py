1adc28c7f6427baee97df30518f3da0e11397a73
dnrkranmol <dnrkranmol@gmail.com>
2024-09-29 10:24:48 +0530

    adding fames

--- calculator_01.py
+++ calculator_01.py
@@ -7,15 +7,15 @@ root.geometry("290x445")
 root.title("Calculator")
 root.attributes("-alpha",0.9)
 
-a=Label (text="This is a sample code by @kr.anmol1234")
+border= Frame(root,bg="gray",borderwidth=2)
+border.pack(side= TOP,fill=X)
+a=Label (border,text="This is a sample code by @kr.anmol1234",bg="gray")
 a.pack()
 photo= PhotoImage(file="logo.png")
 b=Label (image=photo)
 b.pack()
 root.maxsize(1366,768)
 root.minsize(290,445)
-
-
 root.mainloop()
 
 
