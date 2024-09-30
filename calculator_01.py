0071b8e1bf140b977bc973550c0115b52e5439b5
dnrkranmol <dnrkranmol@gmail.com>
2024-09-30 21:19:59 +0530

    adding writhing area

--- calculator_01.py
+++ calculator_01.py
@@ -13,6 +13,7 @@ root.minsize(290,445)
 
 
 
+
 #setting for the labels
 border= Frame(root,bg="gray",borderwidth=2)
 border.pack(side= TOP,fill=X)
@@ -22,28 +23,41 @@ photo= PhotoImage(file="logo.png")
 b=Label (image=photo)
 b.pack(side= RIGHT,anchor="ne")
 
+#creating writing area
+value = Label(root, text="value")
+value.grid(row=0,column=0)
+screen= StringVar()
+scre=Entry(root,textvariable=screen)
+scre.grid(row=0,column=1)
+
+
 #creting buttons
+
+
+
+#frames
 button_label=Frame(root,bg="gray",borderwidth=10)
 button_label.pack()
 
-
-
+#buttons
 b1=Button(button_label,text= "  1  ",fg="red")
-b1.grid(row=0,column=0,padx=10,pady=10)
+b1.grid(row=1,column=0,padx=10,pady=10)
 b2=Button(button_label,text= "  2  ",fg="red")
-b2.grid(row=0,column=1,padx=10,pady=10)
+b2.grid(row=1,column=1,padx=10,pady=10)
 b3=Button(button_label,text= "  3  ",fg="red")
-b3.grid(row=0,column=2,padx=10,pady=10)
+b3.grid(row=1,column=2,padx=10,pady=10)
 b4=Button(button_label,text= "  4  ",fg="red")
-b4.grid(row=0,column=3,padx=10,pady=10)
+b4.grid(row=1,column=3,padx=10,pady=10)
 b5=Button(button_label,text= "  5  ",fg="red")
-b5.grid(row=1,column=0,padx=10,pady=10)
+b5.grid(row=2,column=0,padx=10,pady=10)
 b6=Button(button_label,text= "  6  ",fg="red")
-b6.grid(row=1,column=1,padx=10,pady=10)
+b6.grid(row=2,column=1,padx=10,pady=10)
 b7=Button(button_label,text= "  7  ",fg="red")
-b7.grid(row=1,column=2,padx=10,pady=10)
+b7.grid(row=2,column=2,padx=10,pady=10)
 b8=Button(button_label,text= "  8  ",fg="red")
-b8.grid(row=1,column=3,padx=10,pady=10)
+b8.grid(row=2,column=3,padx=10,pady=10)
+
+
 
 
 
