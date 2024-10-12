49b7d115f00bf965d5efe24d7709a4e3ab587379
dnrkranmol <dnrkranmol@gmail.com>
2024-10-12 23:53:45 +0530

    new buttons

--- calculator_01.py
+++ calculator_01.py
@@ -18,33 +18,51 @@ root.minsize(290,445)
 #creating writing area
 screen= IntVar()
 scre=Entry(root,textvariable=screen)
-scre.grid(row=0,column=0)
+scre.grid(column=2,pady=10,row=1)
 
 
 #creting buttons
 
 
-#frames
-button_label=Frame(root,bg="gray",borderwidth=10)
-button_label.grid()
 
 #buttons
-b1=Button(button_label,text= "  1  ",fg="red")
-b1.grid(row=1,column=1,padx=10,pady=10)
-b2=Button(button_label,text= "  2  ",fg="red")
-b2.grid(row=1,column=2,padx=10,pady=10)
-b3=Button(button_label,text= "  3  ",fg="red")
-b3.grid(row=1,column=3,padx=10,pady=10)
-b4=Button(button_label,text= "  4  ",fg="red")
-b4.grid(row=1,column=4,padx=10,pady=10)
-b5=Button(button_label,text= "  5  ",fg="red")
-b5.grid(row=2,column=1,padx=10,pady=10)
-b6=Button(button_label,text= "  6  ",fg="red")
-b6.grid(row=2,column=2,padx=10,pady=10)
-b7=Button(button_label,text= "  7  ",fg="red")
-b7.grid(row=2,column=3,padx=10,pady=10)
-b8=Button(button_label,text= "  8  ",fg="red")
-b8.grid(row=2,column=4,padx=10,pady=10)
+
+b1=CTkButton(root,text="  1  ",width=4,)
+b1.grid(row=2, column=1, padx=33, pady=20)
+b2=CTkButton(root,text="  2  ",width=4)
+b2.grid(row=2, column=2, padx=33, pady=20)
+b3=CTkButton(root,text="  3  ",width=4)
+b3.grid(row=2, column=3, padx=33, pady=20)
+b4=CTkButton(root,text="  4  ",width=4)
+b4.grid(row=3, column=1, padx=33, pady=20)
+b5=CTkButton(root,text="  5  ",width=4)
+b5.grid(row=3, column=2, padx=33, pady=20)
+b6=CTkButton(root,text="  6  ",width=4)
+b6.grid(row=3, column=3, padx=33, pady=20)
+b7=CTkButton(root,text="  7  ",width=4)
+b7.grid(row=4, column=1, padx=33, pady=20)
+b8=CTkButton(root,text="  8  ",width=4)
+b8.grid(row=4, column=2, padx=33, pady=20)
+b9=CTkButton(root,text="  9  ",width=4)
+b9.grid(row=4, column=3, padx=33, pady=20)
+b0=CTkButton(root,text="  0  ",width=4)
+b0.grid(row=5, column=2, padx=33, pady=20)
+
+add=CTkButton(root,text="  +  ",width=4)
+add.grid(row=5, column=1, padx=33, pady=20)
+
+minus=CTkButton(root,text="  -  ",width=4)
+minus.grid(row=5, column=3, padx=33, pady=20)
+
+into=CTkButton(root,text="  X  ",width=4)
+into.grid(row=6, column=1, padx=33, pady=20)
+
+divide=CTkButton(root,text="  %  ",width=4)
+divide.grid(row=6, column=2, padx=33, pady=20)
+divide=CTkButton(root,text="  =  ",width=4)
+divide.grid(row=6, column=3, padx=33, pady=20)
+
+
 
 
 
